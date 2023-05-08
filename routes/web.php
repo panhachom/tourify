425d233f0312bbaa419480a405c13e83a546f39e
Rmuny <rathmuny85@gmail.com>
2023-05-08 13:56:11 +0700

    close #16-detail-page-ui-design

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\detailpageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
- 
+
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/detailpage', [detailpageController::class, 'index']);
+Route::get('/signup', [HomeController::class, 'signup']);
+
 
 
 /*
@@ -22,6 +26,6 @@ Route::get('/', [HomeController::class, 'index']);
 //     return view('welcome');
 // });
 
-// admin dashbaord Route backend only 
+// admin dashbaord Route backend only
 
 Route::get('/theadmin', [AdminController::class, 'index']);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#D5603F',
-        secondary: '#FFF7F5',
-        third: '#00ABB3',
-        fourth: '#343434',
-        onPrimary: 'rgba(202, 202, 202, 0.2)'
-      },
-      fontFamily: {
-        fontPoppins: ['Poppins']
-      }
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#D5603F',
+                secondary: '#FFF7F5',
+                third: '#00ABB3',
+                fourth: '#343434',
+                onPrimary: 'rgba(202, 202, 202, 0.2)',
+                muny: '#0A3761'
+            },
+            fontFamily: {
+                fontPoppins: ['Poppins']
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
-
