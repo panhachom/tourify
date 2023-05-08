8bb05758a3d994d0d39fed9514325d6c81ccd352
panhachom <panhachom@gmail.com>
2023-05-08 22:35:56 +0700

    Close #26 table Integration

---
+++ app/Models/Tour.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tour extends Model
+{
+    use HasFactory;
+
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
+
+    public function tour_images()
+    {
+        return $this->hasMany(TourImage::class);
+    }
+
+    public function tour_dates()
+    {
+        return $this->hasMany(TourDate::class);
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function activity()
+    {
+        return $this->belongsTo(Activity::class);
+    }
+}
---
+++ database/migrations/2023_05_04_083147_create_tours_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tours', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->float('price'); 
+            $table->integer('capacity');
+        
+            $table->timestamps();
+
+            $table->foreign('vendor_id')->references('id')->on('vendors');
+            $table->unsignedBigInteger('vendor_id');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tours');
+    }
+};
