bac6530a11ed4fc488f0bc1411fd9db7f76fd024
panhachom <panhachom@gmail.com>
2023-05-15 23:20:49 +0700

    Close #63-update-user-management

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,11 +2,7 @@
 @section('title', 'Home')
 @section('content')
 
-<div class="p-4 sm:ml-64">
-        <h1 class="text-red-700">testing</h1>
-        <a href="{{ route('logout.perform') }}" class="btn btn-outline-dark  me-2">Logout</a>
-</div>
-
+<h3>Admin Page</h3>
 
 @endsection
     
